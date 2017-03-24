5fbe58fbbf3fb6f7431af61874cf2499c0fcda93
hedonhermdev <jaintirth24@gmail.com>
2017-03-24 17:47:57 +0530

    Add JSON parser for extracting post data.

--- main.py
+++ main.py
@@ -4,5 +4,14 @@ import urllib.request
 import tkinter as Tk
 import json
 
-
-
+#MEDIA META
+def post_meta(json_script):
+    data = json.loads(json_script)['entry_data']['PostPage'][0]["media"]
+    return {
+        'media' : data['display_src'],
+        'caption' : data['caption'],
+        'date' : data['date'],
+        'num_of_likes' : data['likes'],
+        'num_of_comments' : data['comments']['count'],
+        'comments' : [(c['user']['username'], c['text']) for c in data['comments']['nodes']]
+    }
