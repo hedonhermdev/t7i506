8b122dc13fd97a50ea0ca153be99bac4f9f8af47
hedonhermdev <jaintirth24@gmail.com>
2017-03-25 17:03:03 +0530

    Function for extracting JSON data added.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #IMPORTS
-from bs4 import BeautifulSoup as BS
+import re
+from bs4 import BeautifulSoup
 import urllib.request, urllib.error
 import tkinter as Tk
 import json
-
 #MEDIA META
 def post_meta(json_script):
     data = json.loads(json_script)['entry_data']['PostPage'][0]["media"]
@@ -16,10 +16,15 @@ def post_meta(json_script):
         'comments' : [(c['user']['username'], c['text']) for c in data['comments']['nodes']]
     }
 
-def make_soup(postid):
+def get_data(postid):
     while True:
         try:
             url = urllib.request.urlopen('http://www.instagram.com/p/' + postid) 
-            return BS.soup(url, 'lxml')
-        except (urllib.error.URLError, urllib.error.HTTPError) as e: #FIXME
+            soup = BeautifulSoup(url, 'html.parser')
+            script = list(soup.body.find_all('script'))[2]
+            return str(script.string)[21: -1]
+        except (urllib.error.URLError, urllib.error.HTTPError) as e: #IMPROVE
             return "There was an error. Please try again. Error code : %i" % e.code
+        else:
+            break
+print(get_data('BQ0gGg2jr1W'))
