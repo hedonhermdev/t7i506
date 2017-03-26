d5492b8b3cf7cc6791e15420d9eaab57ed7b1594
hedonhermdev <jaintirth24@gmail.com>
2017-03-26 14:00:00 +0530

    JSON fixed

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #IMPORTS
-import re
 from bs4 import BeautifulSoup
 import urllib.request, urllib.error
 import tkinter as Tk
 import json
 #MEDIA META
-def post_meta(json_script): #DOES NOT HANDLE null VALUES
-    data = json.load(json_script)['entry_data']['PostPage'][0]["media"]
+def post_meta(json_script): 
+    data = json.loads(json_script)['entry_data']['PostPage'][0]["media"]
     return {
         'media' : data['display_src'],
         'caption' : data['caption'],
@@ -20,12 +19,10 @@ def get_data(postid):
     while True:
         try:
             url = urllib.request.urlopen('http://www.instagram.com/p/' + postid) 
-            soup = BeautifulSoup(url, 'html.parser')
-            script = list(soup.body.find_all('script'))[2]
-            return str(script.string)[21: -1]
+            soup = BeautifulSoup(url, 'lxml')
+            return json.loads(str(list(soup.find('body').find_all('script'))[2].string)[21:-1])
         except (urllib.error.URLError, urllib.error.HTTPError) as e: #IMPROVE
-            return "There was an error. Please try again. Error code : %i" % e.code
+            return json.loads("{}")
         else:
             break
-print(post_meta('test.json'))
 
