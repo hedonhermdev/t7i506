ee897c51eec5b8515af2254224a389116aece815
hedonhermdev <jaintirth24@gmail.com>
2017-03-27 17:30:54 +0530

    Everything works.
    Unicode support added. JSON interface minor changes. Exception Handling
    to be improved.

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import urllib.request, urllib.error
 import tkinter as Tk
 import json
 #MEDIA META
-def post_meta(json_script): 
-    data = json.loads(json_script)['entry_data']['PostPage'][0]["media"]
+def post_meta(json_dict):
+    data = json_dict['entry_data']['PostPage'][0]["media"] 
     return {
         'media' : data['display_src'],
-        'caption' : data['caption'],
+        'caption' :u'%s' % data['caption'],
         'date' : data['date'],
         'num_of_likes' : data['likes'],
         'num_of_comments' : data['comments']['count'],
-        'comments' : [(c['user']['username'], c['text']) for c in data['comments']['nodes']]
+        'comments' : [u"@%s : %s" % (c['user']['username'], c['text']) for c in data['comments']['nodes']]
     }
 
 def get_data(postid):
     while True:
         try:
-            url = urllib.request.urlopen('http://www.instagram.com/p/' + postid) 
-            soup = BeautifulSoup(url, 'lxml')
+            url = urllib.request.urlopen('http://www.instagram.com/p/' + postid)
+            soup = BeautifulSoup(url, 'html.parser')
             return json.loads(str(list(soup.find('body').find_all('script'))[2].string)[21:-1])
         except (urllib.error.URLError, urllib.error.HTTPError) as e: #IMPROVE
             return json.loads("{}")
