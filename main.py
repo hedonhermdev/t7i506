8f4b2491e34730e604b5412a05fae3d8071de1f9
hedonhermdev <jaintirth24@gmail.com>
2017-04-03 23:18:54 +0530

    Functions to save metadata and media.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class Post(object):
     def __init__(self, id):
         self.id = id
     # MEDIA META
-
     def post_meta(self):
         try:
             data = self.get_data()['entry_data']['PostPage'][0]["media"]
@@ -22,10 +21,10 @@ class Post(object):
                 'Date': data['date'],
                 'Number of likes': data['likes']['count'],
                 'Number of comments': data['comments']['count'],
-                'Comments': [u"@%s : %s" % (c['user']['username'], c['text']) for c in data['comments']['nodes']]
+                'Comments': [u"@%s : %s" % (c['user']['username'], c['text']) for c in data['comments']['nodes']],
+                'Code' : data['code']
             }
         except KeyError:
-            print("Unexpected error in parsing JSON. Try again. ")
             return 0
 
     def get_data(self):
@@ -40,21 +39,31 @@ class Post(object):
                 for sc in scripts:
                     if re.compile('^window._sharedData').search(str(sc.string)):
                         return json.loads(str(sc.string)[21:-1])
-            except (urllib.error.URLError, urllib.error.HTTPError, ConnectionError) as e:  # IMPROVE!
+            except (urllib.error.URLError, urllib.error.HTTPError, ConnectionError, ) as e:  # IMPROVE!
                 print(str(e))
                 return json.loads("{}")
             else:
                 break
 
+    # Save data in a text file.
+    def writetofile(self, fn):
+        with open(fn, 'w') as f:
+            if self.post_meta():
+                meta = self.post_meta()
+                f.write("***\n\n")
+                # Caption
+                f.write('Caption: %s \n' % meta['Caption'])
+                # Number of Likes
+                f.write('Number of likes: %i \n' % meta['Number of likes'].encode('ascii', 'ignore').decode('windows-1252'))
+                #Number of Comments
+                f.write('Number of comments: %i \n' % meta['Number of comments'])
+                #Comments
+                f.write('Comments: ')
+                for c in meta['Comments']:
+                    s = c.encode('ascii', 'ignore').decode('windows-1252')
+                    f.write(s + '\n')
 
-# MAIN
-print("##########")
-print("Enter the IDs of the posts seperated by commas. Press Enter when done.")
-posts = [id.strip() for id in input().split(',')]
-for i in posts:
-    print("#####")
-    print(i)
-    i = Post(i)
-    meta = i.post_meta()
-    print(meta)
-print("##########")
+    #Save Media locally
+    def save_media(self):
+        if self.post_meta():
+            urllib.request.urlretrieve(self.post_meta()['Media'], self.post_meta()['Code'])
