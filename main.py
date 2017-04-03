fa38da0d33e687bc2dd19504feb84ccbc9eb7ca2
hedonhermdev <jaintirth24@gmail.com>
2017-04-03 12:40:55 +0530

    Major Changes
    -Regex to find JSON script.
    -Posts are now Objects.
    -Can now parse a list of posts from input.

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
-#IMPORTS
-from bs4 import BeautifulSoup, SoupStrainer
-import urllib.request, urllib.error
-import tkinter as Tk
+# pylint: disable=C0321, C0111, C0301, C0103
+# IMPORTS
 import json
-#MEDIA META
-def post_meta(json_dict):
-    data = json_dict['entry_data']['PostPage'][0]["media"] 
-    return {
-        'Media' : data['display_src'],
-        'Caption' :u'%s' % data['caption'],
-        'Date' : data['date'],
-        'Number of likes' : data['likes'],
-        'Number of comments' : data['comments']['count'],
-        'Comments' : [u"@%s : %s" % (c['user']['username'], c['text']) for c in data['comments']['nodes']]
-    }
+import re
+import urllib.error
+import urllib.request
 
-def get_data(postid):
-    while True:
+from bs4 import BeautifulSoup, SoupStrainer
+
+
+class Post(object):
+    def __init__(self, id):
+        self.id = id
+    # MEDIA META
+
+    def post_meta(self):
         try:
-            url = urllib.request.urlopen('http://www.instagram.com/p/' + postid)
-            scripts_only = SoupStrainer('script')
-            soup = BeautifulSoup(url, 'html.parser', parse_only=(scripts_only))
-            print()
-            return json.loads(str(list(soup.find_all('script'))[4].string)[21:-1])
-        except (urllib.error.URLError, urllib.error.HTTPError): #IMPROVE
-            return json.loads("{null:null}")
-        else:
-            break
+            data = self.get_data()['entry_data']['PostPage'][0]["media"]
+            return {
+                'Media': data['display_src'],
+                'Caption': u'%s' % data['caption'],
+                'Date': data['date'],
+                'Number of likes': data['likes']['count'],
+                'Number of comments': data['comments']['count'],
+                'Comments': [u"@%s : %s" % (c['user']['username'], c['text']) for c in data['comments']['nodes']]
+            }
+        except KeyError:
+            print("Unexpected error in parsing JSON. Try again. ")
+            return 0
+
+    def get_data(self):
+        while True:
+            try:
+                url = urllib.request.urlopen(
+                    'http://www.instagram.com/p/' + self.id)
+                scripts_only = SoupStrainer('script')
+                soup = BeautifulSoup(url, 'html.parser',
+                                     parse_only=(scripts_only))
+                scripts = soup.find_all('script', {'type': 'text/javascript'})
+                for sc in scripts:
+                    if re.compile('^window._sharedData').search(str(sc.string)):
+                        return json.loads(str(sc.string)[21:-1])
+            except (urllib.error.URLError, urllib.error.HTTPError, ConnectionError) as e:  # IMPROVE!
+                print(str(e))
+                return json.loads("{}")
+            else:
+                break
+
+
+# MAIN
+print("##########")
+print("Enter the IDs of the posts seperated by commas. Press Enter when done.")
+posts = [id.strip() for id in input().split(',')]
+for i in posts:
+    print("#####")
+    print(i)
+    i = Post(i)
+    meta = i.post_meta()
+    print(meta)
+print("##########")
